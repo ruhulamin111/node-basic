075adb5ba47c7b01da99740d9553df377ade1b69
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-05 19:46:36 +0600

    start node basic

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('working basic node')
+})
+
+const users = [
+    { id: 1, name: 'abu bakar', job: 'developer' },
+    { id: 2, name: 'abu kuddus', job: 'junior developer' },
+    { id: 3, name: 'abu hasem', job: 'senior developer' }
+]
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    const user = users.find(user => user.id == id)
+    res.send(user)
+})
+
+app.listen(port, () => {
+    console.log('working port', port);
+})
